eb9c388262fa0f62907b25281781e0243d91e38e
Supriya Das <41727446+supriyadas13@users.noreply.github.com>
2020-05-16 15:39:01 -0700

    new proj dockerised

---
+++ src/main/java/com/supriya/spring/boot/maven/dockerised/SpringBootMavenDockerisedApplication.java
@@ -0,0 +1,13 @@
+package com.supriya.spring.boot.maven.dockerised;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenDockerisedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMavenDockerisedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supriya/spring/boot/maven/dockerised/SpringBootMavenDockerisedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supriya.spring.boot.maven.dockerised;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenDockerisedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
